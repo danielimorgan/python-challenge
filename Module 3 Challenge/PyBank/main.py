21a99e1a112734b69399dce6b9b35cf15cfb718f
danielimorgan <144614683+danielimorgan@users.noreply.github.com>
2023-12-30 20:32:23 -0500

    Add files via upload

--- Module 3 Challenge/PyBank/main.py
+++ Module 3 Challenge/PyBank/main.py
@@ -1,31 +1,42 @@
-import os
 import csv
 
-#Create Path to dataset
-budget_data = os.path.join('PyBank', 'Resources', 'budget_data.csv')
-print(budget_data)
+#Create Path to datasee
+budget_data = r"C:\Users\danie\OneDrive\Documents\MSU Data Bootcamp Docs\Module 3 - Python\Module 3 Challenge\PyBank\Resources\budget_data.csv"
 with open (budget_data) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     next(csv_reader)
     total_months = 0
     total_profits = 0
+    results = []
 
+    #Find the total number of months in dataset   
     for row in csv_reader:
-        total_months = total_months + 1 
-        total_profits = int(row[1])
-        print(total_profits)
-#Find the total number of months in dataset
-#for row in csv_reader:
- #   months = row[0]
-  #  profits = row[1]
-    
+        month = row[0]
+        profit = row[1]
+        total_months += 1
+        total_profits += int(profit)
+        results.append((month, profit))
 
-#Find the total amount of profit
+average = (total_profits / total_months)
+max_profit = max(results, key=lambda x: int(x[1]))
+min_profit = min(results, key=lambda x: int(x[1]))
+print(max_profit)
 
-#Find the average price transition
 
-#Find the largest increase (date and amount)
-
-#Find the largest decrease (date and amount)
 
 #Print and Export results
+
+output = f'''
+Financial Analysis
+---------------------
+Total Months: {total_months:,}
+Total Profits: {total_profits:,}
+Average Change: {average:.3f}
+Greatest Increase in Profits: {max_profit:}
+Greatest Decrease in Profits: {min_profit:}
+''' 
+
+print(output)
+
+with open('my_report_PyBank.txt', 'w') as f:
+    f.writelines(output)
